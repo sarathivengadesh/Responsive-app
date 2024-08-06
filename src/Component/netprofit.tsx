9d601bd848fa0b97a453a596d740c50aa4483a7c
sarathivengadesh <sarathivengadesh97@gmail.com>
2024-08-06 22:13:50 +0530

    alignment issue

--- src/Component/netprofit.tsx
+++ src/Component/netprofit.tsx
@@ -6,22 +6,24 @@ import { pieChartData } from '../utils/mockdata.tsx'
 export default function NetProfit({totalProfit,status,percent}:{totalProfit:string,status:string,percent:number}) {
 
   return (
-    <div className='p-2 w-full h-full grid grid-cols-2 place-content-between'>
+    <div className='p-2 w-full h-full flex flex-row justify-between'>
      <div className='h-full flex flex-col justify-between'>
         <div className='text1'>Net Profit</div>
         <div className='count'>{totalProfit}</div>
         {trend(status,percent)}
      </div>
-     <div className='w-full mr-0'>
-        <RechartPieChart
-          data={pieChartData}
-          totalCount="70"
-          label1='Goal'
-          label2='Completed'
-          innerRadius={35}
-          outerRadius={45}
-        />
-        <div className='text2 mx-auto'>*The values here has been rounded off.</div>
+     <div className='flex flex-row justify-end'>
+      <div className='w-full mx-0'>
+          <RechartPieChart
+            data={pieChartData}
+            totalCount="70"
+            label1='Goal'
+            label2='Completed'
+            innerRadius={35}
+            outerRadius={45}
+          />
+          <div className='text2 mx-auto'>*The values here has been rounded off.</div>
+      </div>
      </div>
     </div>
   )
